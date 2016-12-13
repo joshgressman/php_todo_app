67a8858685cd6aaafdcb178bff78fa05478c6161
joshgressman <joshgressman@gmail.com>
2016-12-12 19:44:37 -0600

    fixed re-direct

--- inc/get.php
+++ inc/get.php
@@ -9,7 +9,7 @@ if ($result->num_rows > 0) {
     // output data of each row
     // mysqli_result::fetch_assoc -- mysqli_fetch_assoc — Fetch a result row as an associative array
     while($row = $result->fetch_assoc()) {
-        echo "<h3>Task:" . " " . $row["task"]. " " . $row["dat_due"]. " " . "</h3>";
+        echo "<h3>Task:" . " " . $row["task"]. " " . $row["date_due"]. " " . "</h3>";
     }
 } else {
     echo "0 results";
--- inc/post.php
+++ inc/post.php
@@ -7,12 +7,17 @@ $newTask =  $_POST['new_task'];
 $date = $_POST['due_date'];
 $comp = 0;
 
- $sql = "INSERT INTO todo (task, dat_due, completed)
+ $sql = "INSERT INTO todo (task, date_due, completed)
          VALUES ('$newTask', '$date', '$comp')";
       if(mysqli_query($link, $sql)){
+        header("Location: http://localhost:8888/todo_php/index.php"); /* Redirect browser */
         echo "Records added successfully";
+        exit;
       } else {
         echo "ERROR: Could not able to execute" ;
       }
 mysqli_close($link);
+
+
+
  ?>
